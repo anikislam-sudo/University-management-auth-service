9a2454fdf635fbf8f481b1cb8557de73f63df329
unknown <islammoshiu@gmail.com>
2023-06-03 12:59:14 +0600

    user interface and model

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response, urlencoded } from "express"
-import cors from "cors";
-const app:Application = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+//const port = 3000
 
 app.use(cors())
 //parser
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.get('/', (req:Request, res:Response) => {
+app.use(express.urlencoded({ extended: true }))
+app.get('/', (req: Request, res: Response) => {
   res.send('server is working')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-import app from "./app";
-import config from "./config";
-
+import app from './app'
+import config from './config'
 
 //parsee
 
-
 async function main() {
-   
-   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("database is connection successful"); 
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('database is connection successful')
     app.listen(config.port, () => {
-        console.log(` server is listening on port ${config.port}`)
-      })
-
-    } catch (error) {
-         console.log("database is not connection successful",error);
-    }
+      console.log(` server is listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('database is not connection successful', error)
   }
+}
 
-  main();
+main()
